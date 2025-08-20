9dd3aeff3a64a3a18ba16f10a1eef07a10c711ec
EugeneSkool <leewj-wm23@student.tarc.edu.my>
2025-08-20 17:34:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.ck75frghvgws.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "123123123"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "leeweijian-employee "
 customregion = "us-east-1"
 
